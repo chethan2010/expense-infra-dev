919c47fd678c6b4538a7f2de3809033a46878b1c
chethan2010 <hemanthkumarsaketi@gmail.com>
2026-06-08 11:51:29 +0530

    expense-infra-dev

--- 04-db/variables.tf
+++ 04-db/variables.tf
@@ -15,7 +15,9 @@ variable "common_tags" {
 }
 
 variable "zone_name" {
-  default = " mysql -h expense-dev.ccr84yssyn4p.us-east-1.rds.amazonaws.com -u root -pExpenseApp1
+  default = <<EOF
+mysql -h expense-dev.ccr84yssyn4p.us-east-1.rds.amazonaws.com -u root -pExpenseApp1
 mysql: [Warning] Using a password on the command line interface can be insecure.
-ERROR 1045 (28000): Access denied for user 'root'@'10.0.1.230' (using password: YES)"
+ERROR 1045 (28000): Access denied for user 'root'@'10.0.1.230' (using password: YES)
+EOF
 }
